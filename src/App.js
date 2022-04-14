5e15c4eb94b3611d7a7c40caf2d9963f8353175c
Simone <info@simonepozzobon.com>
2022-04-14 08:46:00 +0200

    Ui complete

--- src/App.js
+++ src/App.js
@@ -1,52 +1,59 @@
 import "./App.css";
+import { useRef, useState, useEffect, useCallback } from "react";
 import response from "./Response";
-// import { useRef, useState, useEffect, useCallback } from "react";
 import axios from "axios";
 import {
+    ArrowDownIcon,
     BellIcon,
     DesktopComputerIcon,
+    ExclamationIcon,
+    LocationMarkerIcon,
     UserIcon,
     UsersIcon,
 } from "@heroicons/react/outline";
 
-// function useFetch(query, page) {
-//     const [loading, setLoading] = useState(true);
-//     const [error, setError] = useState(false);
-//     const [list, setList] = useState([]);
+function useFetch(query, page) {
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(false);
+    const [list, setList] = useState([]);
+    const [max, setMax] = useState(0);
+    const limit = 3;
+    const sendQuery = useCallback(async () => {
+        try {
+            console.log((page - 1) * limit);
+            await setLoading(true);
+            await setError(false);
 
-//     const sendQuery = useCallback(async () => {
-//         try {
-//             await setLoading(true);
-//             await setError(false);
-//             const res = await axios.get(
-//                 "https://dev-api.confidence.org/v2/confidence/locations",
-//                 {
-//                     headers: {
-//                         "Content-Type": "application/json",
-//                         "Access-Control-Allow-Origin": "http://localhost:3000",
-//                     },
-//                     auth: {
-//                         username: "amitphatak$r5labs.com",
-//                     },
-//                     // body: {
-//                     //     start: 0,
-//                     //     limit: 10,
-//                     // },
-//                 }
-//             );
-//             await setList((prev) => [...prev, ...res.data]);
-//             setLoading(false);
-//         } catch (err) {
-//             setError(err);
-//         }
-//     }, [query, page]);
+            const data = JSON.stringify({ start: (page - 1) * limit, limit });
+            const config = {
+                method: "post",
+                url: "/v2/confidence/locations",
+                headers: {
+                    Username: "amitphatak$r5labs.com",
+                    "Content-Type": "application/json",
+                },
+                data: data,
+            };
+            const res = await axios(config);
+            console.log("res.data=>", res.data);
 
-//     useEffect(() => {
-//         sendQuery(query);
-//     }, [query, sendQuery, page]);
+            setMax(res.data.numberOfLocations);
+            if (max >= list.length) {
+                await setList((prev) => [...prev, ...res.data.locations]);
+            }
 
-//     return { loading, error, list };
-// }
+            setLoading(false);
+        } catch (err) {
+            setError(err);
+        }
+    }, [query, page]);
+
+    useEffect(() => {
+        sendQuery(query);
+    }, [query, sendQuery, page]);
+
+    return { loading, error, list };
+}
 
 function LocationCard(props) {
     const { location } = props;
@@ -75,29 +82,33 @@ function LocationCard(props) {
     }
 
     return (
-        <li className="bg-white shadow-md overflow-hidden rounded-md border border-light-blue flex gap-4 lg:gap-6">
+        <li className="bg-white shadow-md overflow-hidden rounded-md border border-light-blue flex">
             <div className="flex flex-col items-center bg-white shadow-inner w-16 overflow-hidden">
                 <div className="lg:hidden w-full">{subscriptionBadge}</div>
-                <div class="flex-grow flex items-center gap-2 px-4 py-2 bg-orange-100 shadow-inner">
+                <div className="flex-grow flex items-center gap-2 px-4 py-2 bg-orange-100 shadow-inner">
                     <UserIcon className="w-4 text-gray-600" />
                     <span className="text-gray-600">
-                        {location.numberofMyTasks}
+                        {location.numberofMyTasks
+                            ? location.numberofMyTasks
+                            : 0}
                     </span>
                 </div>
-                <div class="flex-grow flex items-center gap-2 px-4 py-2 bg-blue-100 shadow-inner">
+                <div className="flex-grow flex items-center gap-2 px-4 py-2 bg-blue-100 shadow-inner">
                     <UsersIcon className="w-4 text-gray-600" />
                     <span className="text-gray-600">
-                        {location.numberofTasks}
+                        {location.numberofTasks ? location.numberofTasks : 0}
                     </span>
                 </div>
                 <div className="flex-grow flex items-center gap-2 px-4 py-2 bg-purple-100 shadow-inner">
                     <DesktopComputerIcon className="w-4 text-gray-600" />
                     <span className="text-gray-600">
-                        {location.numberofDevices}
+                        {location.numberofDevices
+                            ? location.numberofDevices
+                            : 0}
                     </span>
                 </div>
             </div>
-            <div className="flex-grow py-4">
+            <div className="flex-grow py-4 border-l border-gray-100 px-4">
                 <div className="block lg:hidden">{badge}</div>
                 <div className="text-gray-500 font-light text-xs mt-1 lg:mt-0">
                     {location.locationUserRole}
@@ -117,8 +128,12 @@ function LocationCard(props) {
                     <div className="hidden lg:block">{badge}</div>
                 </div>
                 <div className="text-gray-500 mt-1">
+                    <LocationMarkerIcon className="w-4 text-gray-400 hidden lg:inline-block mr-1" />
                     <span className="block lg:inline-block">
                         {location.address.addressLine1},&nbsp;
+                    </span>
+                    <span className="block lg:inline-block">
+                        {location.address.addressLine2},&nbsp;
                     </span>
                     <span className="block lg:inline">
                         {location.address.city},&nbsp;
@@ -135,54 +150,66 @@ function LocationCard(props) {
 
 function App() {
     const { locations } = response;
-    console.log(locations);
-    // const [query, setQuery] = useState("");
-    // const [page, setPage] = useState(1);
-    // const { loading, error, list } = useFetch(query, page);
-    // const loader = useRef(null);
-
-    // const handleChange = (e) => {
-    //     setQuery(e.target.value);
-    // };
-
-    // const handleObserver = useCallback((entries) => {
-    //     const target = entries[0];
-    //     if (target.isIntersecting) {
-    //         setPage((prev) => prev + 1);
-    //     }
-    // }, []);
-
-    // useEffect(() => {
-    //     const option = {
-    //         root: null,
-    //         rootMargin: "20px",
-    //         threshold: 0,
-    //     };
-    //     const observer = new IntersectionObserver(handleObserver, option);
-    //     if (loader.current) observer.observe(loader.current);
-    // }, [handleObserver]);
+    const loading = false;
+    const error = false;
+    const loader = null;
 
     return (
         <div className="min-h-screen bg-gradient-to-br from-light-blue to-dark-blue">
-            <header className="text-center py-16">
-                <h1 className="text-4xl font-extrabold tracking-wider text-gray-900/90">
+            <header className="text-center pt-16">
+                <h1 className="text-4xl font-extrabold tracking-wider text-white/90">
                     LOCATIONS
                 </h1>
             </header>
             {/* <input type="text" value={query} onChange={handleChange} /> */}
-            <div className="max-w-3xl mx-auto py-4 px-4 sm:py-6 sm:px-6 md:py-8 md:px-8 lg:px-0 lg:py-24">
+            <div className="max-w-3xl mx-auto py-4 px-4 sm:py-6 sm:px-6 md:py-8 md:px-8 lg:px-0 lg:py-16">
                 <ul className="space-y-4">
-                    {locations.map((location) => (
-                        <LocationCard
-                            key={location.locationId}
-                            location={location}
-                        />
+                    {locations.map((location, i) => (
+                        <LocationCard key={i} location={location} />
                     ))}
                 </ul>
             </div>
-            {/* {loading && <p>Loading...</p>}
-            {error && <p>Error!</p>}
-            <div ref={loader} /> */}
+
+            {loading ? (
+                <div className="text-white flex justify-center items-center pb-24">
+                    <svg
+                        className="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                    >
+                        <circle
+                            className="opacity-25"
+                            cx="12"
+                            cy="12"
+                            r="10"
+                            stroke="currentColor"
+                            strokeWidth="4"
+                        ></circle>
+                        <path
+                            className="opacity-75"
+                            fill="currentColor"
+                            d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                        ></path>
+                    </svg>
+                    <span className="text-sm">Loading</span>
+                </div>
+            ) : (
+                <div className="flex justify-center items-center pb-24">
+                    <ArrowDownIcon className="w-4 text-white animate-bounce" />
+                </div>
+            )}
+
+            {error && (
+                <div className="flex justify-center items-center pb-24 relative">
+                    <div className="inline-flex items-center px-4 py-2 rounded-full text-xs font-medium bg-red-500 text-white shadow-xl">
+                        <ExclamationIcon className="w-4 text-white" />
+                        <span className="ml-2">Error Loading !</span>
+                    </div>
+                </div>
+            )}
+
+            <div ref={loader} />
         </div>
     );
 }
